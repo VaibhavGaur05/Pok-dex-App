80141c5d2e72dd11e130072724664b76a474dff7
Vaibhav Gaur <64890655+VaibhavGaur05@users.noreply.github.com>
2024-10-28 22:35:28 +0530

    refactor

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/6`
+    for(let i=1; i<=150; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
     fetch(url)
         .then(res => {
             return res.json()
         })
         .then(data => {
                 console.log(data);
-                const pokemon = {};
-                pokemon['name'] = data.name;
-                pokemon['id'] = data.id;
-                pokemon['image'] = data.sprites['front_default'];
-                pokemon['type'] = data.types
-                                    .map((insideElement)=>insideElement.type.name)
-                                    .join(', ')
+                const pokemon = {
+                    name: data.name,
+                    id: data.id,
+                    image: data.sprites['front_default'],
+                    type:  data.types
+                                .map((insideElement)=>insideElement.type.name)
+                                .join(', ')   
+
+                };
                 console.log(pokemon);
         })
+
+    }
+    
 }
 
 fetchPokemon();
