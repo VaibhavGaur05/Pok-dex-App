a641258bc0e5f50600f56c930114f2e09020ddbd
Vaibhav Gaur <64890655+VaibhavGaur05@users.noreply.github.com>
2024-11-12 01:24:51 +0530

    details and popups
    changed the way details are displayed.
    design changes still pending for popups.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const fetchPokemon = async () => {
 
 
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
+    
     const pokemonHTML = pokemon.map( p => `
         <li class="card" onclick="selectPokemon(${p.id})">
             <img class="card=image" src="${p.image}"/>
@@ -33,8 +33,43 @@ const displayPokemon = (pokemon) => {
 }
 
 const selectPokemon = async (id) => {
-    console.log(id);
+   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+   const res = await fetch(url);
+   const pokemonData = await res.json();
+    // this is a callback funcn  which will be passed as an argument in displayPopup() later on..
+   displayPopup(pokemonData);
 } 
+
+const displayPopup = (pokemonData) => {
+    const image = pokemonData.sprites['front_default']
+    const type = pokemonData.types.map( element => element.type.name).join(',');
+    const htmlString = `
+        <div class="popup">
+            <button id="closeBtn" onclick="closePopup()">
+                close
+            </button>
+             <div class="card">
+            <img class="card=image" src="${image}"/>
+            <h2 class="card-title">${pokemonData.id}. ${pokemonData.name}</h2>
+            <p><small>Height: </small>${pokemonData.height} | <small>Weight: </small>${pokemonData.weight} |
+             <small>Type: </small>${type}
+
+            </div>    
+
+
+
+        </div> 
+    `;
+    pokedex.innerHTML = htmlString +pokedex.innerHTML;
+
+};
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup)
+
+}
+
 
 
 fetchPokemon();
